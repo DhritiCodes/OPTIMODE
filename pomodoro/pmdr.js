6c38435c9b8c4b8609fb809b9e855f4f4620e577
dhriti <dhritideepagorai@gmail.com>
2022-01-08 20:25:47 +0530

    commiting to upload

---
+++ pomodoro/pmdr.js
@@ -0,0 +1,13 @@
+// pomodoro definition - on click show
+var showDef = document.querySelector(".definition");
+var def = document.querySelector("pmdr-defn p")
+showDef.addEventListener("click", function() {
+    console.log("something");
+    def.classList.add("hidden");
+
+
+});
+
+// function showDefinition {
+//     alert("SUccess");
+// }
